da0b24449ca47b571a9fe4b486dff00fe47b981c
linyuan <691029538@qq.com>
2020-08-17 10:56:25 +0800

    修改图片上传url

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -67,7 +67,7 @@ module.exports = app =>{
     const upload = multer({dest:__dirname+'/../../uploads'})
     app.post('/admin/api/upload',authMiddleware(),upload.single('file'), async(req,res)=>{
         const file = req.file
-        file.url = `http://localhost:8080/uploads/${file.filename}`
+        file.url = `http://test.alinyuan.com/uploads/${file.filename}`
         res.send(file)
     })
 
